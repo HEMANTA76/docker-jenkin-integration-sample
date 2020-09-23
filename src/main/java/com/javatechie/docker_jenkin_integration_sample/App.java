73cbb9a83d871bf08d1d3891c61a46e3f0996f05
sasmita <sasmeeta.swain@gmail.com>
2020-09-22 22:12:52 -0400

    my first commit

---
+++ src/main/java/com/javatechie/docker_jenkin_integration_sample/App.java
@@ -0,0 +1,13 @@
+package com.javatechie.docker_jenkin_integration_sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
